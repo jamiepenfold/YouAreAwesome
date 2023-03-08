c1ede0b730213f4bb3ac440e182711c21ecc50a3
Jamie Penfold <jamiepenfold3@gmail.com>
2023-03-08 13:50:23 +0200

    ch1.5

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,28 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = ""
+    @State private var messageString = "Yo!"
     var body: some View {
-        VStack{
-            Text(messageString)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundColor(.red)
-            HStack{
-                Button("Great") {
-                    // this is the action performed when the button is pressed
-                    messageString = "You are Great!"
+            VStack{
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+                    .border(.orange, width: 1)
+                    .padding()
+                HStack{
+                    Button("Great") {
+                        // this is the action performed when the button is pressed
+                        messageString = "You are Great!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    Button("Awesome") {
+                        // this is the action performed when the button is pressed
+                        messageString = "You are Awesome!"
+                    }
+                    .buttonStyle(.borderedProminent)
                 }
-                .buttonStyle(.borderedProminent)
-                Button("Awesome") {
-                    // this is the action performed when the button is pressed
-                    messageString = "You are Awesome!"
-                }
-                .buttonStyle(.borderedProminent)
+                .border(.purple, width: 5)
             }
         }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
