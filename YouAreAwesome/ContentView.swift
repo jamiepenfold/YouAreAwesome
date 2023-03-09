2b218b08c43ac1d19c4af00797f3bd4754d3561d
Jamie Penfold <jamiepenfold3@gmail.com>
2023-03-09 22:21:05 +0200

    ch 1.7

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,48 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = "Yo!"
+    @State private var messageString = ""
     var body: some View {
+        GeometryReader {geometry in
             VStack{
-                    Text("You have skills!")
-                        .font(.largeTitle)
-                        .fontWeight(.black)
-                        .foregroundColor(Color("Gold-BC"))
-                        .padding()
-                        .background(Color("Maroon-BC"))
-                        .cornerRadius(15)
-                    Spacer()
-                    Text(messageString)
-                        .font(.largeTitle)
-                        .fontWeight(.heavy)
-                        .minimumScaleFactor(0.5)
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(.red)
-                        .frame(height: 150)
-                        .frame(maxWidth: .infinity)
-    //                    .border(.orange, width: 1)
-                        .padding()
-                    
-                    Spacer()
-                    HStack{
-                        Button("Great") {
-                            // this is the action performed when the button is pressed
-                            messageString = "You are Great!"
-                        }
-                        .buttonStyle(.borderedProminent)
-                        Spacer()
-                        Button("Awesome") {
-                            // this is the action performed when the button is pressed
-                            messageString = "You are Awesome!"
-                        }
-                        .buttonStyle(.borderedProminent)
-                    }
-    //                .border(.purple, width: 5)
+                Spacer()
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+                //                    .border(.orange, width: 1)
                     .padding()
-                    .tint(Color("Vermillion"))
+                
+                Spacer()
+                
+                Divider()
+                    .background(.black)
+                    .padding()
+                    .frame(width: 150.0)
+                
+                Rectangle()
+                    .fill(.indigo)
+                    .frame(width: geometry.size.width * (2/3) , height: 1)
+                
+                HStack{
+                    Button("Great") {
+                        // this is the action performed when the button is pressed
+                        messageString = "You are Great!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    Spacer()
+                    
+                    Button("Awesome") {
+                        // this is the action performed when the button is pressed
+                        messageString = "You are Awesome!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+                //                .border(.purple, width: 5)
+                .padding()
+                .tint(Color("Vermillion"))
             }
         }
     }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
